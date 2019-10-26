1d293d0d826db91aee6aa8fd609ad0a887854ba0
olga <olga-p-98@mail.ru>
2019-10-26 14:12:02 +0300

    added main() function

--- quora_question_search/search_engine/views.py
+++ quora_question_search/search_engine/views.py
@@ -20,3 +20,27 @@ from keras.models import Model
 from keras_bert.layers import MaskedGlobalMaxPool1D
 from keras_bert import load_trained_model_from_checkpoint
 from keras_bert import Tokenizer, load_locabulary, get_checkpoint_paths
+
+def main(request):
+    query = request.GET["query"]
+    model = request.GET["model"]
+    results = []
+    failed_search = False
+    if query and model:
+        if model == "tf_idf":
+            engine = TfIdfSearch()
+        elif model == "bm25":
+            engine = BM25Search()
+        elif model == "word2vec":
+            engine = Word2VecSearch()
+        elif model == "fasttext":
+            engine = FastTextSearch()
+        elif model == "elmo":
+            engine = ELMOSearch()
+        elif model == "bert":
+            engine = RuBERTSearch()
+        results = engine.search(query)
+        if results is None:
+            failed_search = True
+    return render(request, "main.html", {"results": results, 
+                  "failed_search": failed_search})

49b57b5551ed27a1080cd2bf5877debb2880cbed
olga <olga-p-98@mail.ru>
2019-10-26 14:16:46 +0300

    added urls.py

--- quora_question_search/search_engine/views.py
+++ quora_question_search/search_engine/views.py
@@ -21,6 +21,7 @@ from keras_bert.layers import MaskedGlobalMaxPool1D
 from keras_bert import load_trained_model_from_checkpoint
 from keras_bert import Tokenizer, load_locabulary, get_checkpoint_paths
 
+
 def main(request):
     query = request.GET["query"]
     model = request.GET["model"]
@@ -42,5 +43,5 @@ def main(request):
         results = engine.search(query)
         if results is None:
             failed_search = True
-    return render(request, "main.html", {"results": results, 
+    return render(request, "main.html", {"results": results,
                   "failed_search": failed_search})

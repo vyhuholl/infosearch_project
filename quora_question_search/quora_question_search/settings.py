e50ac636a6224284eedbeb0b15cb316493a33b33
olga <olga-p-98@mail.ru>
2019-10-26 14:27:14 +0300

    edited settings.py

--- quora_question_search/quora_question_search/settings.py
+++ quora_question_search/quora_question_search/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')ggb$$pxj7rt38_&h6a_g7-#l(%r+nqit)dk63&sx6khee88l9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'search_engine'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'quora_question_search.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'quora_question_pairs_rus.db'),
     }
 }
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
